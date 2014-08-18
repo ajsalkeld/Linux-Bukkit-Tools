00a1125c381dee5f51896693e57c59bdb4677a46
AJ - Lanky Nerd <Andrew@Pollux.local>
2014-08-18 21:08:32 +0100

    fix

--- installer.sh
+++ installer.sh
@@ -8,14 +8,14 @@
 echo "Do you have a Bukkit install already? (y/n)"
 read bukkitinstallyn
 
-if test $bukkitinstallyn == "y" || test $bukkitinstallyn == "Y" 
+if [[ $bukkitinstallyn == "y" ]]
 then
   echo "Where is the start.sh/launch.sh for your Bukkit install?"
   read bukkitDir
   echo "Setting up..."
   sed -i "12i bukkitDir=$bukkitDir" bkstart.sh
 fi
-if test $bukkitinstallyn == "n" || test $bukkitinstallyn == "N" 
+if [[ $bukkitinstallyn == "n" ]]
 then
   echo "Where would you like Bukkit to be installed?"
   read bukkitInstallDir
